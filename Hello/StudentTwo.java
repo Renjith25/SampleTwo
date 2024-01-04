45c7ceedf55a59d4c3daf41a4710f536649306ae
Renjith25 <renjithkumar.muthu@gmail.com>
2024-01-04 18:40:52 +0530

    login

---
+++ Hello/StudentTwo.java
@@ -0,0 +1,24 @@
+package org.info;
+
+public abstract class StudentTwo extends StudentOne {
+	public void studentGrade() {
+		System.out.println('A');
+	}
+	public void studentPhone() {
+		System.out.println(26542654l);
+	}
+	public void studentAge() {
+		System.out.println(20);
+	}
+	public void studentAddress() {
+		System.out.println("Chennai");
+	}
+	public void studentid() {
+		System.out.println(92345);
+	}
+	public void studentEmail() {
+		System.out.println("jack@1234");
+	}
+
+
+}
