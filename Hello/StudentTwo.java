babc2cf360f20e3556a1507c6132528168b90acc
Renjith25 <155149629+Renjith25@users.noreply.github.com>
2024-01-04 23:34:38 +0530

    Update StudentTwo.java

--- Hello/StudentTwo.java
+++ Hello/StudentTwo.java
@@ -1,24 +1,4 @@
-package org.info;
-
-public abstract class StudentTwo extends StudentOne {
-	public void studentGrade() {
-		System.out.println('A');
-	}
-	public void studentPhone() {
-		System.out.println(26542654l);
-	}
-	public void studentAge() {
-		System.out.println(20);
-	}
-	public void studentAddress() {
-		System.out.println("Chennai");
-	}
-	public void studentid() {
-		System.out.println(92345);
-	}
-	public void studentEmail() {
-		System.out.println("jack@1234");
-	}
-
-
-}
+<center>
+	<h1> vijay </h1>
+	</center>
+	<hr>
